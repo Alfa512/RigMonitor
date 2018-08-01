dd86197f0153b6f09ff13f66cb5ec4d190a3a347
ALFA <alfa512ks@mail.ru>
2018-08-01 18:35:31 +0300

    RigMonitor.Services added

---
+++ RigMonitor.Services/RigMonitorService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RigMonitor.Api.Nanopool.ETH;
+
+namespace RigMonitor.Services
+{
+    public class RigMonitorService
+    {
+        public EthController EthController;
+
+        public RigMonitorService()
+        {
+            EthController = new EthController();
+        }
+
+        public async void RestartByWatchDogIfNoReport(string workerId, int targetHashrate)
+        {
+            var workersData = EthController.GetAllWorkersData();
+            var worker = workersData.FirstOrDefault(r => r.Id.ToLower().Equals(workerId.ToLower()));
+
+        }
+    }
+}
